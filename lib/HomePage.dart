c899b11218ede4510d7182e6f1666bc40cba5e95
azzamaflah <azzamhebat46@gmail.com>
2025-04-25 15:30:26 +0700

    membuat menu pada tampilan home

---
+++ lib/HomePage.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.exit_to_app),
+            onPressed: () {
+              // Implement your logout functionality here
+            },
+          ),
+        ],
+        title: const Text('Selamat Datang'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+    
+
+            // Action buttons section
+            GridView.count(
+              shrinkWrap: true,
+              crossAxisCount: 3,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 5,
+              children: [
+                buildActionButton(context, 'Data Piket', Icons.chat),
+                buildActionButton(context, 'Data Pelanggan', Icons.person),
+                buildActionButton(context,'Barang Masuk/Keluar',Icons.inventory,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildActionButton(BuildContext context, String title, IconData icon) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(vertical: 20), backgroundColor: Colors.orange,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      ),
+      onPressed: () {
+        // Add navigation logic here for each button
+        print('$title clicked');
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, size: 30, color: Colors.white),
+          const SizedBox(height: 10),
+          Text(title, style: const TextStyle(color: Colors.white)),
+        ],
+      ),
+    );
+  }
+}
